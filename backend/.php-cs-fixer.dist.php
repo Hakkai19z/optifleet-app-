8cfcc3ab90bbd518cd7ec9127aa61ae6d07abdaf
Kahil Mokhtari <kahil.mokhtari@gmail.com>
2026-06-01 11:47:11 +0200

    chore(backend): add PHP CS Fixer and PHPStan static analysis configuration

---
+++ backend/.php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('migrations')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => false,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'trailing_comma_in_multiline' => true,
+        'phpdoc_order' => true,
+        'void_return' => true,
+        'concat_space' => ['spacing' => 'one'],
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(false)
+;
